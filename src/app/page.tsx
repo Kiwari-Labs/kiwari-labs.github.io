105c08945a3eabff11cadc681678a9ec4029aa70
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-11-11 15:02:42 +0700

    feat: add section frequentlyAskedQuestion and HowDoesItWork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Footer } from "@/components/Footer";
+import { FrequentlyAskedQuestions } from "@/components/FrequentlyAskedQuestions";
+import { HowDoesItWork } from "@/components/HowDoesItWork";
 
 export default function Home() {
   return (
-    <div className=" min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col container mx-auto">
+      <HowDoesItWork />
+      <FrequentlyAskedQuestions />
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => {
   return (
-    <footer className="mt-auto w-full py-4">
-      <p className="text-[#a2a2a2] text-center">Copyright © 2024 Kiwari Labs</p>
+    <footer className="mt-auto w-full h-full py-4 bg-[#1c1c1c] rounded-t-3xl">
+      <div className="flex flex-col w-full items-center py-10">
+        <h2 className="text-2xl md:text-5xl font-extrabold text-center">
+          Ready to get started?
+        </h2>
+        <p className="text-sm md:text-lg text-center leading-relaxed md:first-letter w-full px-5 md:px-0 md:w-1/2 mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+        </p>
+      </div>
+      <div className="flex justify-center w-full">
+        <Link href={"https://github.com/Kiwari-Labs"}>
+          <Image
+            src="/assets/github.png"
+            alt="Faq image"
+            width={25}
+            height={25}
+          />
+        </Link>
+      </div>
     </footer>
   );
 };
