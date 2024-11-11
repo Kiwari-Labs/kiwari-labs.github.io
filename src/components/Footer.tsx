9d723db2b7c0a127a961e7f54455f45a09b8edbd
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-11-11 11:31:47 +0700

    fix: layout and dependency

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-auto w-full py-4">
+      <p className="text-[#a2a2a2] text-center">Copyright © 2024 Kiwari Labs</p>
+    </footer>
+  );
+};
