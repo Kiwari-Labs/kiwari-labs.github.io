91b4a9854c7aca8a0ff08e05885648b02cf2e3bb
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-11-12 11:35:31 +0700

    feat: add hero and feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import { Feature } from "@/components/Feature";
 import { Footer } from "@/components/Footer";
 import { FrequentlyAskedQuestions } from "@/components/FrequentlyAskedQuestions";
+import { Hero } from "@/components/Hero";
 import { HowDoesItWork } from "@/components/HowDoesItWork";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col container mx-auto">
-      <HowDoesItWork />
-      <FrequentlyAskedQuestions />
-      <Footer />
+    <div className="min-h-screen flex flex-col">
+      <Hero />
+
+      <div className="flex flex-col container mx-auto">
+        <Feature />
+        <HowDoesItWork />
+        <FrequentlyAskedQuestions />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/HowDoesItWork.tsx
+++ src/components/HowDoesItWork.tsx
@@ -1,26 +1,31 @@
 export const HowDoesItWork = () => {
   return (
-    <div className="flex flex-col w-full h-full py-10 md:py-24">
-      <div className="flex flex-col w-full md:px-0">
-        <h2 className="text-xl md:text-2xl lg:text-5xl font-extrabold text-center pb-5 pt-5 md:pt-0">
+    <div className="flex flex-col w-full h-full py-10 gap-10 pb-10 md:py-24">
+      <div className="flex flex-col w-full md:px-0 items-center">
+        <h2 className="text-xl md:text-2xl lg:text-5xl font-extrabold text-center pb-10 md:pt-0">
           How does it work?
         </h2>
 
-        <p className="mt-4 leading-relaxed text-[#a2a2a2] text-center px-5 md:px-0 text-sm md:text-base">
-          Macros are quick actions that you can run with Design Maestro
-          launcher. To launch a macro you just need to choose it from the list
-          or activate it with an assigned keyboard shortcut.
+        <p className="mt-4 leading-relaxed text-[#a2a2a2] text-center px-5 md:px-0 text-sm md:text-base w-full md:w-4/5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore
         </p>
 
-        <div className="flex flex-col md:flex-row h-full mt-5">
+        <div className="flex flex-col md:flex-row h-full mt-5 gap-5">
           <div className="flex flex-col text-center items-center py-5 md:py-20 gap-y-5">
             <div className="flex items-center justify-center px-5 py-5 w-5 h-5 bg-[#FFFFFF26] rounded-full">
               <p className="font-bold text-lg">1</p>
             </div>
-            <p className="font-extrabold font-lg">Install Keyboard Maestro</p>
+            <p className="font-extrabold font-lg">Lorem ipsum dolor</p>
             <p className="text-[#a2a2a2] px-5 md:px-0 text-sm md:text-base">
-              Design Maestro is an extension for the Keyboard Maestro app.
-              First, you need to install it on your Mac.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore
             </p>
           </div>
 
@@ -28,10 +33,13 @@ export const HowDoesItWork = () => {
             <div className="flex items-center justify-center px-5 py-5 w-5 h-5 bg-[#FFFFFF26] rounded-full">
               <p className="font-bold text-lg">2</p>
             </div>
-            <p className="font-extrabold font-lg">Install Design Maestro</p>
+            <p className="font-extrabold font-lg">Lorem ipsum dolor</p>
             <p className="text-[#a2a2a2] px-5 md:px-0 text-sm md:text-base">
-              Download the Design Maestro extension from our website and install
-              it by double-clicking on the file.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore
             </p>
           </div>
 
@@ -39,10 +47,13 @@ export const HowDoesItWork = () => {
             <div className="flex items-center justify-center px-5 py-5 w-5 h-5 bg-[#FFFFFF26] rounded-full">
               <p className="font-bold text-lg">3</p>
             </div>
-            <p className="font-extrabold font-lg">Start using the macros</p>
+            <p className="font-extrabold font-lg">Lorem ipsum dolor</p>
             <p className="text-[#a2a2a2] px-5 md:px-0 text-sm md:text-base">
-              Launch the Design Maestro with ⌘⇧D and choose the macro you want
-              to activate.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore
             </p>
           </div>
         </div>
